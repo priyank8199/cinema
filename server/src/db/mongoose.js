1c0afc560e4d318eeff2aa54edd1acf343bc6d16
Priyank Patel <pr371749@dal.ca>
2023-12-02 10:48:19 -0400

    initial

---
+++ server/src/db/mongoose.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+  console.log('MongoDB database connection established successfully');
+});
+
+connection.on('error', (err) => {
+  console.error('MongoDB connection error: ', err);
+});
